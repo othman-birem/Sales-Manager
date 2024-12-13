aa6a26c0f0405594b82eb7d345f634f2bc3bead1
othman birem <othman.milano18@gmail.com>
2024-12-13 21:23:12 +0100

    Charged assets and started designing UI/UX

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Sales_Manager.ViewModels;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
 
 namespace Sales_Manager
 {
@@ -14,6 +16,28 @@ namespace Sales_Manager
             InitializeComponent();
             DataContext = VM;
             viewModel = VM;
+        }
+
+        private void MainFrame_Navigating(object sender, System.Windows.Navigation.NavigatingCancelEventArgs e)
+        {
+            Page frame = (Page)((Frame)sender).Content;
+            if (frame == null) return;
+            frame.Opacity = 0;
+
+            Storyboard storyboard = new();
+            DoubleAnimation animation = new()
+            {
+                Duration = new Duration(TimeSpan.FromMilliseconds(100)),
+                From = 1,
+                To = 0
+            };
+
+            storyboard.Children.Add(animation);
+
+            Storyboard.SetTarget(animation, frame);
+            Storyboard.SetTargetProperty(animation, new PropertyPath(Page.OpacityProperty));
+
+            storyboard.Begin();
         }
     }
 }
